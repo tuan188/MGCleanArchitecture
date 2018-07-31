ce209a2c64bcb443979b2ac52104b0909605de16
Tuan Truong <tuan188@gmail.com>
2018-07-31 16:26:59 +0700

    Refactor load more paging

--- CleanArchitecture/Architecture/ViewModel/ViewModelType+Loadmore.swift
+++ CleanArchitecture/Architecture/ViewModel/ViewModelType+Loadmore.swift
@@ -15,75 +15,20 @@ extension ViewModelType {
             loading: Driver<Bool>,
             refreshing: Driver<Bool>,
             loadingMore: Driver<Bool>) {
-            
-            let pageSubject = BehaviorRelay<PagingInfo<T>>(value: PagingInfo<T>(page: 1, items: []))
-            let errorTracker = ErrorTracker()
-            let loadingActivityIndicator = ActivityIndicator()
-            let refreshingActivityIndicator = ActivityIndicator()
-            let loadingMoreActivityIndicator = ActivityIndicator()
-            
-            let loading = loadingActivityIndicator.asDriver()
-            let refreshing = refreshingActivityIndicator.asDriver()
-            let loadingMore = loadingMoreActivityIndicator.asDriver()
-            
-            let loadingOrLoadingMore = Driver.merge(loading, refreshing, loadingMore)
-                .startWith(false)
-            
-            let loadItems = loadTrigger
-                .withLatestFrom(loadingOrLoadingMore)
-                .filter { !$0 }
-                .flatMapLatest { _ in
-                    getItems()
-                        .trackError(errorTracker)
-                        .trackActivity(loadingActivityIndicator)
-                        .asDriverOnErrorJustComplete()
-                }
-                .do(onNext: { page in
-                    pageSubject.accept(page)
-                })
-                .mapToVoid()
-            
-            let refreshItems = refreshTrigger
-                .withLatestFrom(loadingOrLoadingMore)
-                .filter { !$0 }
-                .flatMapLatest { _ in
-                    refreshItems()
-                        .trackError(errorTracker)
-                        .trackActivity(refreshingActivityIndicator)
-                        .asDriverOnErrorJustComplete()
-                }
-                .do(onNext: { page in
-                    pageSubject.accept(page)
-                })
-                .mapToVoid()
-            
-            let loadMoreItems = loadMoreTrigger
-                .withLatestFrom(loadingOrLoadingMore)
-                .filter { !$0 }
-                .filter { _ in !pageSubject.value.items.isEmpty }
-                .flatMapLatest { _ -> Driver<PagingInfo<T>> in
-                    let page = pageSubject.value.page
-                    return loadMoreItems(page + 1)
-                        .trackError(errorTracker)
-                        .trackActivity(loadingMoreActivityIndicator)
-                        .asDriverOnErrorJustComplete()
-                }
-                .filter { !$0.items.isEmpty }
-                .do(onNext: { page in
-                    let currentPage = pageSubject.value
-                    let items: OrderedSet<T> = currentPage.items + page.items
-                    let newPage = PagingInfo<T>(page: page.page, items: items)
-                    pageSubject.accept(newPage)
-                })
-                .mapToVoid()    
-            
-            let fetchItems = Driver.merge(loadItems, refreshItems, loadMoreItems)
-            return (pageSubject,
-                    fetchItems,
-                    errorTracker.asDriver(),
-                    loading,
-                    refreshing,
-                    loadingMore)
+                
+        return setupLoadMorePagingWithParam(
+            loadTrigger: loadTrigger,
+            getItems: { _ in
+                return getItems()
+            },
+            refreshTrigger: refreshTrigger,
+            refreshItems: { _ in
+                return refreshItems()
+            },
+            loadMoreTrigger: loadMoreTrigger,
+            loadMoreItems: { _, page in
+                return loadMoreItems(page)
+            })
     }
 }
 
